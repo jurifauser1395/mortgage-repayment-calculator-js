9c186337ce35d0bbe97d000a13adf32c09c873c8
jurifauser1395 <139368246+jurifauser1395@users.noreply.github.com>
2023-11-03 16:00:01 +0100

    implement js mortgage calculation

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-let principalLoanAmount;
-let monthlyInterestRate;
-let numberOfMonthlyPayments;
-let numberOfYearsPayments;
+//Initiate the program
+document.getElementById("dataEntrance").addEventListener("click", function () {
+//Get input values
+    let principalLoan = parseFloat(document.getElementById("principalLoan").value);
+    let monthlyInterestRate = parseFloat(document.getElementById("monthlyInterestRate").value);
+    let numberOfYearsPayments = parseFloat(document.getElementById("numberOfYearsPayments").value);
+    let numberOfMonthlyPayments = numberOfYearsPayments * 12;
 
-principalLoanAmount = 500000;
-monthlyInterestRate = 5;
-numberOfYearsPayments = 30;
-numberOfMonthlyPayments = numberOfYearsPayments * 12;
+    //Calculate the monthly payment
+    function monthlyMortgagePayment(P, r, n) {
+        let M;
+        let x = r / 100 / 12
+        M = (P * x) / (1 - Math.pow(1 + x, -n));
+        return M.toFixed(2);
+    }
+    //Store result in a variable
+    const monthlyPayment = monthlyMortgagePayment(principalLoan, monthlyInterestRate, numberOfMonthlyPayments);
+    //Sass the result into the browser
+    document.getElementById("result").textContent = "$" + monthlyPayment;
+});
 
-function monthlyMortgagePayment(P, r, n) {
-    let M;
-    let x = r/100/12
-    M = (P * x) / (1 - Math.pow(1 + x, -n));
-    return M.toFixed(2);
-}
 
-const monthlyPayment = monthlyMortgagePayment(principalLoanAmount, monthlyInterestRate, numberOfMonthlyPayments);
-console.log("$" + monthlyPayment);
+
+
