8956f9652d9fdc75e6f9c23b999abd8d321af2d1
jurifauser1395 <139368246+jurifauser1395@users.noreply.github.com>
2023-11-05 16:33:16 +0100

    rearange and optimize code, implement the total interest payed and total payment calculation

--- app.js
+++ app.js
@@ -1,65 +1,89 @@
-//Define variables
-let principalLoan;
-let monthlyInterestRate;
-let numberOfYearsPayments;
-
-//Define the monthlyMortgagePayment function to calculate the monthly payment to the second decimal number
+//Declare the monthlyMortgagePayment function to calculate the monthly payment to the second decimal number
 function monthlyMortgagePayment(P, r, n) {
     let M;
-    let x = r / 100 / 12
+    let x = r / 100 / 12;
     M = (P * x) / (1 - Math.pow(1 + x, -n));
     return M.toFixed(2);
 }
+//Declare the monthlyAmortizationSchedule function to display all costs in detail
+/*function monthlyAmortizationSchedule (x) {
+    if (x <= 0) {
+        let tableResult = document.getElementById("tableResult")
+        tableResult.textContent = "0"
+    } else {
+
+        let tableResult = document.getElementById("tableResult")
+        tableResult.textContent = principalLoan
+    }
+}*/
 
 //Initiate the program
-let calculationButton = document.getElementById("calculateButton")
+let calculationButton = document.getElementById("calculateButton");
 calculationButton.addEventListener("click", outputCalResult);
 
 //Define the outputCalResult function
 function outputCalResult (){
 
+    //Define variables
+    let principalLoan;
+    let monthlyInterestRate;
+    let numberOfYearsPayments;
+
     //initialise calculation variables
-    principalLoan = parseFloat(document.getElementById("principalLoan").value);
-    monthlyInterestRate = parseFloat(document.getElementById("monthlyInterestRate").value);
-    numberOfYearsPayments = parseFloat(document.getElementById("numberOfYearsPayments").value);
-    let totalNumberOfPayments;
-    totalNumberOfPayments = numberOfYearsPayments * 12;
+    principalLoan = document.getElementById("principalLoan").value;
+    monthlyInterestRate = document.getElementById("monthlyInterestRate").value;
+    numberOfYearsPayments = document.getElementById("numberOfYearsPayments").value;
+    let totalNumberOfPayments = numberOfYearsPayments * 12;
 
-    //call monthlyMortgagePayment function to store result in a variable
-    const monthlyPayment = monthlyMortgagePayment(principalLoan, monthlyInterestRate, totalNumberOfPayments);
+    // Clear all exclamation marks initially
+    let error1 = document.getElementById("error1");
+    let error2 = document.getElementById("error2");
+    let error3 = document.getElementById("error3");
+    error1.textContent = "";
+    error2.textContent = "";
+    error3.textContent = "";
 
-    //check if the calculated value is NaN
-    if(isNaN(monthlyPayment)) {
-
-        //Printout error message in the result tag
-        let result = document.getElementById("result");
-        result.textContent = "Please make sure all fields are filled with plausible numbers";
-
-        //Printout exclamation mark right next to the field
-        if(isNaN(principalLoan)) {
-            let error1 = document.getElementById("error1");
-            error1.textContent = "!"
-        }
-
-        //Printout exclamation mark right next to the field
-        if(isNaN(monthlyInterestRate)) {
-            let error2 = document.getElementById("error2");
-            error2.textContent = "!"
-        }
-
-        //Printout exclamation mark right next to the field
-        if(isNaN(numberOfYearsPayments)) {
-            let error3 = document.getElementById("error3");
-            error3.textContent = "!"
-        }
-
+    //Printout exclamation mark right next to the field if entry is missing or wrong
+    if(isNaN(principalLoan)) {
+        let error1 = document.getElementById("error1");
+        error1.textContent = "!";
     }
 
-    //Pass the result into the browser
-    else{
-        //START PROGRAMMING HERE NEXT TIME
-        let result =  document.getElementById("result");
-        result.textContent = "$" + monthlyPayment;
+    //Printout exclamation mark right next to the field if entry is missing or wrong
+    if(isNaN(monthlyInterestRate)) {
+        let error2 = document.getElementById("error2");
+        error2.textContent = "!";
+    }
+
+    //Printout exclamation mark right next to the field if entry is missing or wrong
+    if(isNaN(numberOfYearsPayments)) {
+        let error3 = document.getElementById("error3");
+        error3.textContent = "!";
+    }
+
+    //Call monthlyMortgagePayment function to store result in a variable
+    let monthlyPayment = monthlyMortgagePayment(principalLoan, monthlyInterestRate, totalNumberOfPayments);
+
+    //Calculate total interest paid and total payment
+    let totalPayment = monthlyPayment * totalNumberOfPayments;
+    let totalInterestPaid = totalPayment - principalLoan;
+
+    //Pass the results into the browser
+    if(!isNaN(monthlyPayment)) {
+        //Pass the result of the monthly payments
+        let resultMonthlyPayment =  document.getElementById("resultMonthly");
+        resultMonthlyPayment.textContent = "$" + monthlyPayment;
+        //Pass the result of the total interest paid
+        let resultTotalInterestPayed = document.getElementById("resultTotalInterestPaid");
+        resultTotalInterestPayed.textContent = "$" + totalInterestPaid.toFixed(2);
+        //Pass the result of the total payment
+        let resultTotalPayment = document.getElementById("resultTotalPayment");
+        resultTotalPayment.textContent = "$" + totalPayment.toFixed(2);
+
+    } else {
+        //Printout error message in the result tag
+        let result = document.getElementById("resultMonthly");
+        result.textContent = "Please make sure all fields are filled with plausible numbers!";
     }
 }
 
